6a7ff25a1062611fd3f1803dc2fc9f80ac8025a6
arturtugui <146761295+arturtugui@users.noreply.github.com>
2026-05-03 00:26:25 +0300

    fix: prevent blank app screen

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -4,7 +4,6 @@ import ItemCard from './components/ItemCard'
 import './styles/App.css'
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, { items: [] })
 
   return (
     <div className="app-container">
